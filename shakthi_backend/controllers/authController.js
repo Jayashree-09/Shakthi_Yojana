2553265aa2e488cc91d32bf66e1ef645202f14bd
Jayashree <jayashree@example.com>
2026-04-30 11:15:49 +0530

    fix email SMTP port to 587

--- shakthi_backend/controllers/authController.js
+++ shakthi_backend/controllers/authController.js
@@ -7,8 +7,19 @@ const axios = require("axios");
 const otpStore = {};
 
 // ✅ EMAIL TRANSPORTER
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// ✅ EMAIL TRANSPORTER
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
